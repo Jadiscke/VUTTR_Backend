23d21460ce9f68ede9222966159706964d1bbcb8
Vinicius Jadiscke <vjtasso@gmail.com>
2020-12-07 15:07:10 -0300

    Create Query Filter

--- src/controllers/toolsController.ts
+++ src/controllers/toolsController.ts
@@ -4,7 +4,21 @@ import Tool from "../model/Tool";
 class ToolController {
   async getAll(request: Request, response: Response): Promise<Response<any>> {
     try {
-      const tools = await Tool.find({});
+      if (request.query.tag) {
+        request.query = {
+          ...request.query,
+          tags: request.query.tag,
+        };
+        delete request.query.tag;
+      }
+      if (request.query?.tags) {
+        if (Number(request.query?.tags?.length) > 1) {
+          const tags: any = request.query.tags || [""];
+          const tools = await Tool.find().where("tags").all(tags);
+          return response.status(200).json(tools);
+        }
+      }
+      const tools = await Tool.find().where(request.query);
       return response.status(200).json(tools);
     } catch (error) {
       return response.status(400).json({
